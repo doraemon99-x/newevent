bc46ce2c164edf4d33976cd8e86fb0d311daa257
doraemon99-x <75873265+doraemon99-x@users.noreply.github.com>
2023-11-28 20:36:20 +0700

    update

--- player.js
+++ player.js
@@ -23,6 +23,7 @@ $(document).ready(function(){
                             '4d38060bf41b3c29df0ec950ece6b5da': '7ee9506b13480491d79b71c062ab5366',
                             '9ba3e153ef8956d6e2b0684fcf74f58f': 'dbc28cb5c6426080f984a5b6d436bb30',
                             '57d2ac9210cfbca3596cc679a01c8b29': 'd5e35c0f39c76adf24853d7ea18c71e7',
+                            'c9f3795d0a1d46b505fd5b5d34953dec': 'ef3bb480e631339ce3690d519f328fbd',
                         }
                     }
                 });
